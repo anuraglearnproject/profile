9b8dfb5aff5cf07b7e5dc57af5ded63b388c5098
anuraglearnproject <anuraglearnproject@gmail.com>
2025-05-07 00:58:12 +0530

    change live url

--- anuragreview/src/services/login.service.ts
+++ anuragreview/src/services/login.service.ts
@@ -3,18 +3,17 @@ import { Injectable } from '@angular/core';
 import { LoginDto } from '../models/login.dto';
 import { LoginResponseDto } from '../models/login-response.dto';
 import { Observable } from 'rxjs';
+import { APP_CONSTANTS } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  private readonly apiUrl = 'http://localhost:5243/api/login';
-
   constructor(private http: HttpClient) { }
 
   loginRequest(credentials: LoginDto): Observable<LoginResponseDto> {
-    return this.http.post<LoginResponseDto>(this.apiUrl, credentials, {
+    return this.http.post<LoginResponseDto>(APP_CONSTANTS['localhost.API_BASE_URL'] + 'Login', credentials, {
       withCredentials: true
     });
   }
